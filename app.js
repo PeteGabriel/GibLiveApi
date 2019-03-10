11040288b1feed4eefa05da4db8633413f8b7beb
Pedro Gabriel <pedrogabriel@protonmail.ch>
2019-03-10 22:46:12 +0100

    Add class action to represent a top level arrival/departure

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
+//Routers
 const flightRouter = require('./1presentation/routers/flights_router')
 
+//Middleware registration
 app.use(flightRouter.routes());
+
+
 
 app.listen(3000);
--- src/1presentation/routers/FlightsRouter.ts
+++ src/1presentation/routers/FlightsRouter.ts
@@ -2,14 +2,22 @@ import * as Router  from 'koa-router';
 const router = new Router();
 
 
-router.get('/', (ctx: any, next: Function) => {
+router.get('/live', (ctx: any, next: Function) => {
   ctx.body = 'hello'
   ctx.status = 200
   return next()
 })
 
+router.get('/live/departures', (ctx: any, next: Function) => {
+  ctx.body = 'departures'
+  ctx.status = 200
+  return next()
+})
 
-
-
+router.get('/live/arrivals', (ctx: any, next: Function) => {
+  ctx.body = 'arrivals'
+  ctx.status = 200
+  return next()
+})
 
 export default router
