c1a66557f0d4c06c895e928129f628f4a55a5832
Pedro Gabriel <pedrogabriel@protonmail.ch>
2019-03-16 22:53:19 +0100

    Add chi package and first route

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", handleRoot)
+	http.ListenAndServe(":3000", r)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome"))
+}
