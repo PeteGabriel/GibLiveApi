92d317bdb2d1e72e9b8d72085db8a4f76751eae2
Pedro Almeida <Pedro.PitadeAlmeida@betvictor.com>
2019-06-17 14:12:27 +0200

    Setup the service and controller api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './presentation/app.controller';
 import { AppService } from './domain/app.service';
+import { Crawler } from './domain/service.crawler';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Crawler],
 })
 export class AppModule {}
--- src/presentation/app.controller.ts
+++ src/presentation/app.controller.ts
@@ -1,18 +1,25 @@
 import { Controller, Get, HttpCode, Header } from '@nestjs/common';
 import { AppService } from '../domain/app.service';
+import { Crawler } from 'src/domain/service.crawler';
 
 @Controller('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly cService: Crawler
+    ) {}
 
-  
-
+  @Get('/departures')
+  @HttpCode(200)
+  @Header('Content-Type', 'application/json')
+  async departures(): Promise<string> {
+    return JSON.stringify(await this.cService.getDeparturesInfo())
+  }
 
   @Get('alive')
   @HttpCode(200)
   @Header('Content-Type', 'application/json')
   alive(): string {
-
     return this.appService.alive();
   }
 }
